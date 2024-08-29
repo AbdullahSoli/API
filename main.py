09c8f402fc674103d0a86bb17379486732056f79
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-29 06:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def root():
- return "Player Preductions"
+ return "Player Preduction"
 @app.get("/items/{item_id}")
 async def read_item(item_id):
  return {"item_id": item_id}
