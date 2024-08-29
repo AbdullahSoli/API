791c15ec08b921cd070cbb316117feacc3e548be
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-29 04:12:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def root():
- return "Welcome To Tuwaiq Academy"
+ return "Welcome To Tuwaiq Academ"
 @app.get("/items/{item_id}")
 async def read_item(item_id):
  return {"item_id": item_id}
